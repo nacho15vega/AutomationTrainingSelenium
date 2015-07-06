f0e9d6523c3ba002d056dababdb8c0e405a58e18
nacho15vega <nacho15vega@gmail.com>
2015-07-06 16:16:07 -0600

    Grid

--- gridexamples/EjemploGrid.java
+++ gridexamples/EjemploGrid.java
@@ -1,71 +1,62 @@
 package gridexamples;
 
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
-
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class EjemploGrid {
-	public WebDriver driver;
-	 
-	  @Parameters("browser")
-	 
-	  @BeforeClass
-	   public void beforeTest(String browser) {
+    private static RemoteWebDriver driver;
+    
+    @Parameters("browser")
+    
+    @BeforeClass
+	   public void setUP(String browser) throws MalformedURLException {
 	 
 	  // If the browser is Firefox, then do this
 	 
 	  if(browser.equalsIgnoreCase("firefox")) {
-	 
-		  driver = new FirefoxDriver();
+	 		DesiredCapabilities capability = DesiredCapabilities.firefox();
+		    driver = new RemoteWebDriver(new URL("http://localhost:1234/wd/hub"), capability);
 	 
 	  // If browser is IE, then do this	  
 	 
 	  }else if (browser.equalsIgnoreCase("ie")) { 
 	 
 		  // Here I am setting up the path for my IEDriver
-	 
-		  System.setProperty("webdriver.ie.driver", "/Users/avega/Desktop/Drivers/IEDriverServer.exe");
-	 
-		  driver = new InternetExplorerDriver();
+	 		DesiredCapabilities capability = DesiredCapabilities.internetExplorer();
+		  	//System.setProperty("webdriver.ie.driver", "/Users/avega/Desktop/Drivers/IEDriverServer.exe");
+	 		driver = new RemoteWebDriver(new URL("http://localhost:1234/wd/hub"), capability);
+		  
 	 
 	  } else if(browser.equalsIgnoreCase("chrome")){
 		  
-		  System.setProperty("webdriver.chrome.driver", "/Users/avega/Desktop/Drivers/chromedriver.exe");
-		  driver = new ChromeDriver();
+		  DesiredCapabilities capability = DesiredCapabilities.chrome();
+		  driver = new RemoteWebDriver(new URL("http://localhost:1234/wd/hub"), capability);
 
 	  }
 	 
 	  // Doesn't the browser type, launch the Website
 	 
-	  driver.get("https://avansecu-qa:8181/SecurityTestingSite/"); 
+	  driver.get("https://avansecu-qa:8181/SecurityTestingSite/");
 	 
 	  }
-	  
-	  @AfterMethod
-	  public void afterMethod() {
-
-		  driver.close();
-		  
-	  }
-
-	  
-	  
-	  @Test
-	  public void successfullLogin(){
-		  driver.get("https://avansecu-qa:8181/SecurityTestingSite/");
-		  new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("formLogin:ingresar")));
+    
+    @Test
+    public void successfullLogin() {
+    	 new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(By.id("formLogin:ingresar")));
 		  Assert.assertEquals(driver.getTitle(),"Clasificados Avantica");
 		 
 		  String username = "ccolon";
@@ -80,7 +71,11 @@ public class EjemploGrid {
 		  button.click();
 		  //new WebDriverWait(driver,30).until(ExpectedConditions.elementToBeClickable(By.className("tituloEspecial")));
 		  Assert.assertEquals(driver.getTitle(),"Inicio");
-	  }
-	
+    }
+    
+    @AfterMethod
+    public void close(){
+    	driver.close();   	
+    }
 	
 }
